5e03211a5d39ec3d45326787732afd3f1e092bec
lecoding111 <lecoding111@gmail.com>
2023-11-19 14:21:00 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const resetButton = document.getElementById('resetButton');
+    
+    resetButton.addEventListener('click', function () {
+        updatePageOpenTime();
+    });
+
+    function updatePageOpenTime() {
+        localStorage.pageOpenTime = new Date().getTime();
+        alert("Page open time updated to the current time.");
+    }
+
+    const savedPageOpenTime = localStorage.pageOpenTime;
+    const countDownDate = new Date("2023-11-28T00:00:00").getTime();
+    const pageLoadTime = savedPageOpenTime ? parseInt(savedPageOpenTime) : new Date().getTime();
+
+    const countdownInterval = setInterval(function () {
+        const now = new Date().getTime();
+        const elapsedTime = now - pageLoadTime;
+        const remainingTime = countDownDate - now;
+
+        const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = `${days}D ${hours}H ${minutes}M ${seconds}S`;
+        document.getElementById("progress-bar").style.width = `${(elapsedTime / (countDownDate - pageLoadTime)) * 100}%`;
+
+        if (remainingTime < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("countdown").innerHTML = "Happy Birthday!";
+            document.getElementById("progress-bar").style.width = "100%";
+        }
+    }, 100);
+});
